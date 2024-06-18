1cd1965ef985ce1ff84145e8928b4fafa6a471dd
Linux-K-Git <76905355+Linux-K-Git@users.noreply.github.com>
2024-06-18 11:33:20 +0800

    Add files via upload

---
+++ 两行代码实现贪吃蛇，小熊猫即可打开.c
@@ -0,0 +1,4 @@
+#include <windows.h>
+#include <conio.h>
+int main(){int W=20,S=W*W,*m,z[2]={0},l=3,i,c='D',C,*p,f;SetConsoleCursorInfo(GetStdHandle((DWORD)-11), &(CONSOLE_CURSOR_INFO){25, 0});srand(m = calloc(S, 4));C = m[1] = -1;while(C!=27){Sleep(100);if (_kbhit()){C = _getch() & 95, C - 65 && C - 68 && C - 83 && C - 87 || (C ^ c) & 20 ^ 4 && (c = C);}p = z + !!(c & 2);*p += c / 3 & 2;if (--*p < 0 || *p >= W) C = 27;else{f = 1, * (p = m + *z + z[1] * W) > 0 && (C = 27);for (; *p && (m[i = rand() % S] || (--m[i], ++l, --f)););SetConsoleCursorPosition(GetStdHandle((DWORD)-11), (COORD){0});for (i = 0, *p = l; i < S; _cputs("  "), ++i % W || _cputs("\n")){SetConsoleTextAttribute(GetStdHandle((DWORD)-11),m[i] > 0 ? m[i] -= f, 192 : m[i] ? 160 : 240);}}}free(m);return 0;}
+
